b4854cc36705083e67849677a1d71e6d6d8356f1
Michael <mathematik@habmalnefrage.de>
2021-05-17 21:19:20 +0200

    working on cross platform (added executable path);

--- src/bleep.cpp
+++ src/bleep.cpp
@@ -5,11 +5,14 @@
 /// \brief Provides the main function for this game.
 //==============================================================================
 
-#ifdef _WIN32
-    #define USE_WINDOWS
-#endif
 #define OLC_PGE_APPLICATION
 #define OLC_PGEX_SOUND
+
+#ifdef _WIN32
+    #define USE_WINDOWS
+#else
+    #include <unistd.h>
+#endif
 
 #include <rapidjson/document.h>
 
@@ -21,6 +24,45 @@
 
 int main()
 {
+    //get the current path of the executable platform independently
+    std::size_t len{};
+    std::wstring currentPath{};
+
+    #ifdef USE_WINDOWS
+    {
+        std::vector<wchar_t> buffer(0);
+        do
+        {
+            buffer.resize(buffer.size() + 1);
+            len = GetModuleFileName(NULL, &buffer.at(0), buffer.size());
+        } while (buffer.size() == len);
+
+        if (len == 0)
+            ;//throw
+        else
+            currentPath = { buffer.data(), buffer.size() };
+
+        std::wcout << currentPath;
+    }
+    #else
+    {
+        std::vector<char> buffer(0);
+        do
+        {
+            buffer.resize(buffer.size() + 1);
+            len = readlink("/proc/self/exe", &buffer.at(0), buffer.size());
+        } while (buffer.size() == len);
+
+        if (len == 0)
+            ;//throw
+        else
+        {
+            std::string path{ buffer.data(), buffer.size() };
+            currentPath = { path.begin(), path.end() }
+        }
+    }
+    #endif
+
     //setting up rand() - that rng is good enough for this purpose
     srand(static_cast<unsigned int>(time(0)));
 
